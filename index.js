fae52c9b0043c48a58ede78a0e19166e6bc08a0f
asib ahmed <ahmedasib1@gmail.com>
2025-01-12 18:44:10 +0600

    feat #3 : use base url

--- index.js
+++ index.js
@@ -2,30 +2,18 @@ const express = require('express');
 
 const app = express();
 
-// app.use(express.static(`${__dirname}/public/`,{
-//     extensions: ['html']
-// }));
+const adminRoute = express.Router();
+adminRoute.get('/dashboard', (req, res) =>{
+    console.log(req.baseUrl)
+    res.send('this is dashboard')
+})
 
-// app.get('/', (req, res) => {
-//     res.send('THIS is home page');
-// });
+app.use('/admin', adminRoute)
 
-// app.post('/', (req, res) => {
-//     res.send('This is login page');
-// });
-
-app.set('view engine', 'ejs');
-
-app.route('/about/mission')
-    .get((req, res) => {
-        res.render('pages/about');
-    })
-    .post((req, res) => {
-        res.send('This is about mission page post');
-    })
-    .put((req, res) => {
-        res.send('This is about mission page put');
-    })
+app.get('/user/:id', (req, res) =>{
+    console.log(req.baseUrl);
+    res.send('hello')
+})
 
 app.listen(3000, () => {    
     console.log('Server is running on port 3000');
