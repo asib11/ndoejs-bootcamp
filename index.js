ebd83aa77fbd0b5afb8e1cc5dcaee618140e8b66
asib ahmed <ahmedasib1@gmail.com>
2025-01-12 20:55:44 +0600

    feat: baseurl, originalurl, url

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 
 const adminRoute = express.Router();
 adminRoute.get('/dashboard', (req, res) =>{
-    console.log(req.baseUrl)
+    console.log(req.baseUrl) // /admin
+    console.log(req.originalUrl) // /admin/dashboard
+    console.log(req.url) // /dashboard
     res.send('this is dashboard')
 })
 
@@ -12,6 +14,8 @@ app.use('/admin', adminRoute)
 
 app.get('/user/:id', (req, res) =>{
     console.log(req.baseUrl);
+    console.log(req.originalUrl); // /user/2?filter=name
+    console.log(req.url); // /user/2?filter=name
     res.send('hello')
 })
 
